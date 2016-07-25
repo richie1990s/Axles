b5426d4581de7e2b055fc16f693c17d5fdeb7c65
shulq <shulq@liansu.com>
2016-07-25 14:35:45 +0800

    项目初始导入

---
+++ AxleDemo/src/main/java/com/richie/axles/AxleFunctionsActivity.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2016  richie (richie1990@163.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.richie.axles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Axle功能列表(包含已支持和未支持)
+ * @author richie (richie1990@163.com)
+ */
+public class AxleFunctionsActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_axle_functions);
+    }
+}
